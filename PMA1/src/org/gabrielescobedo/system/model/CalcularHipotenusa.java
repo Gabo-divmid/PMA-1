ffccb6b0f27c13074d1ad47a70b3a4a23fdd51ec
Diego Escobedo <gescobedo-2023401@kinal.edu.gt>
2026-06-09 19:14:55 -0600

    006 se agrego la Calcular Hipotenusa

---
+++ PMA1/src/org/gabrielescobedo/system/model/CalcularHipotenusa.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.gabrielescobedo.system.model;
+import javax.swing.JOptionPane;
+
+public class CalcularHipotenusa {
+      public static void main(String[] args) {
+
+        double catetoA = Double.parseDouble(JOptionPane.showInputDialog("Ingrese Cateto A:"));
+        double catetoB = Double.parseDouble(JOptionPane.showInputDialog("Ingrese Cateto B:"));
+
+        double hipotenusa = Math.sqrt(Math.pow(catetoA, 2) + Math.pow(catetoB, 2));
+
+        JOptionPane.showMessageDialog(null, "Hipotenusa: " + hipotenusa);
+    }
+}
